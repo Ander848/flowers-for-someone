5d5bf36f2f5d3281ec15143d16520bec7e1b2fad
Ander848 <andervalencia848@gmail.com>
2025-02-01 23:29:59 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = '¿Quieres ser mi enamorada este 14 de febrero?'.split('')
+const text = ' ¿Quieres ser mi enamorada este 14 de febrero?'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += <span>${text[index]}<span/>
