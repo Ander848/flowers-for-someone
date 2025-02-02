bf85de3ce58a8ee752f72799ef8971801d526de7
Ander848 <andervalencia848@gmail.com>
2025-02-01 23:40:42 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,15 @@
-const title = document.querySelector('.title');
-const text = '¿Quieres ser mi enamorada este 14 de febrero?'.split('');
-
+const title = document.querySelector('.title')
+const text = '¿Quieres ser mi enamorada este 14 de febrero?'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
-    title.innerHTML += `<span>${text[index]}</span>`; // Corrección aquí
+    title.innerHTML += <span>${text[index]}<span/>
   } else {
-    title.innerHTML += `<span style='margin-right: 5px;'></span>`; // Espacio con menor margen
+    title.innerHTML += <span style='margin-right: 20px;'><span/>
   }
 }
 
 const textElements = document.querySelectorAll('.title span');
 textElements.forEach((element) => {
-  const randomDelay = Math.random() * 3; // Genera un retraso aleatorio
-  element.style.animationDelay = `${randomDelay}s`;
+  const randomDelay = Math.random() * 3; // Menghasilkan delay acak antara 0 hingga 3 detik
+  element.style.animationDelay = ${randomDelay}s;
 });
