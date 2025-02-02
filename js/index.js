0d27f8cc932f7c7cbd3bbd3a542fc041d2e1a1f9
Ander848 <andervalencia848@gmail.com>
2025-02-01 22:59:05 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'De parte de tu noviecito Ander <br> ¿Quisieras ser mi enamorada este 14 de febrero?'.split('')
+const text = 'De parte de tu noviecito Ander <br> ¿Quieres ser mi enamorada este 14 de febrero?'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
